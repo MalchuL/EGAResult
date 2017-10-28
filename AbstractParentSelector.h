d0f6464f8e1436a37337cc19a8e10ed3ff64b1c5
Vlad Sorokin <malchugan666@gmail.com>
2017-10-29 01:59:49 +0300

    update parent selector

--- AbstractParentSelector.h
+++ AbstractParentSelector.h
@@ -2,8 +2,10 @@
 #include <utility>
 #include <vector>
 #include "ByteVector.h"
+#include "ByteVectorMath.h"
 #include <algorithm>
 #include "SetSizeException.h"
+#include "Roulette.h"
 using namespace std;
 typedef std::pair<ByteVector, ByteVector> BytePair;
 class AbstractParentSelector
@@ -14,12 +16,27 @@ public:
 		if (byteVectorList.size() < 2)throw SetSizeException();
 		random_shuffle(byteVectorList.begin(), byteVectorList.end());
 		ByteVector firstParent(byteVectorList.at(0));
-		ByteVector secondParent = SelectPair(firstParent,byteVectorList);
-
+		ByteVector secondParent = SelectPairToFirstVector(firstParent,byteVectorList);
+		return BytePair(firstParent, secondParent);
 
 	}
-
-	virtual ByteVector SelectPair(ByteVector firstParent, vector<ByteVector>& potencialParents) = 0;
+	virtual float Probability(ByteVector firstParent, ByteVector secondParent) = 0;
+	ByteVector SelectPairToFirstVector(ByteVector firstParent,const vector<ByteVector>& potencialParents) {
+		vector<ByteVector> parents;
+		vector<float> probabilities;
+		for(ByteVector vector:potencialParents)
+		{
+			int length = ByteVectorMath::HemmingLength(firstParent, vector);
+			if (length > 0) {
+				parents.push_back(vector);
+				probabilities.push_back(Probability(firstParent, vector));
+			}
+		}
+		Roulette<ByteVector> roulette = Roulette<ByteVector>(parents, probabilities);
+		ByteVector secondParent = roulette.getRandomObject();
+		return secondParent;
+	
+	}
 	~AbstractParentSelector();
 };
 
